5284f57d189a0eabb545f88904771dcb070e8bc1
pedromessetti <pedromessetti@gmail.com>
2023-04-23 17:06:58 +0100

    add files

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,35 @@
 /*   By: pmessett <pmessett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 12:17:04 by pmessett          #+#    #+#             */
-/*   Updated: 2023/04/18 12:22:23 by pmessett         ###   ########.fr       */
+/*   Updated: 2023/04/23 17:04:23 by pmessett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-  
+	static char	buf[BUFFER_SIZE + 1];
+	char		*line;
+	int			i;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	line = ft_strjoin(0, buf);
+	if (ft_clear(buf))
+		return (line);
+	i = read(fd, buf, BUFFER_SIZE);
+	if (i < 0)
+	{
+		free(line);
+		return (NULL);
+	}
+	while (i > 0)
+	{
+		line = ft_strjoin(line, buf);
+		if (ft_clear(buf))
+			break ;
+		i = read(fd, buf, BUFFER_SIZE);
+	}
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,25 @@
 /*   By: pmessett <pmessett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 12:17:07 by pmessett          #+#    #+#             */
-/*   Updated: 2023/04/18 12:22:14 by pmessett         ###   ########.fr       */
+/*   Updated: 2023/04/23 08:01:28 by pmessett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
 # include <stdlib.h>
-char *get_next_line(int fd);
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_clear(char *s);
 
 #endif
